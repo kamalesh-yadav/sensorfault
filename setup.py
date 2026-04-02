7c8c9043c4638f1d677e2288190d4854aa2c6af2
kamalesh-yadav <studentpw2@gmail.com>
2026-04-02 14:10:38 +0530

    everything is fine

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+from typing import List
+setup(
+    name='sensor',
+    version="0.0.1",
+    author='kamalesh',
+    author_email='kamaleshysy123@gmail.com',
+    packages=find_packages(),
+
+)
